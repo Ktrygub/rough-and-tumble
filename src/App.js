424560439a6b33a6abcdfed975dfbca304caa2fa
Ktrygub <konstantin.trygub@gmail.com>
2018-01-15 23:58:16 +0200

    CRA + Eslint/Prettier

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Rough&Tumble</h1>
+        </header>
+      </div>
+    )
+
+export default App
